08015144825f40e024d09eaad85ec896a4cc6d6c
jarconett <67648532+jarconett@users.noreply.github.com>
2025-09-06 23:01:01 +0200

    Update coord.py

--- coord.py
+++ coord.py
@@ -57,8 +57,7 @@ nombres_archivos = []
 territorios_file = None
 
 for archivo in uploaded_files:
-    #if archivo.name.lower() == "territorios.csv":
-    if archivo.lower() == "territorios.csv":
+    if archivo.name.lower() == "territorios.csv":
         territorios_file = archivo
         continue
     try:
