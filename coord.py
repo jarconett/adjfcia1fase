7faf849345217289a4fbedb650ee40c1290a4640
jarconett <67648532+jarconett@users.noreply.github.com>
2025-09-06 23:21:02 +0200

    Update coord.py

--- coord.py
+++ coord.py
@@ -94,7 +94,7 @@ st.sidebar.subheader("Carga y Geolocalización")
 #archivo_coords = st.sidebar.file_uploader(
 #    "Sube el archivo CSV con coordenadas (ej. ieca_export_latitud_longuitud.csv)", type='csv'
 #)
-archivo_coords = pd.read("ieca_export_latitud_longuitud.csv")
+archivo_coords = pd.read_csv("ieca_export_latitud_longuitud.csv")
 if 'df_coords' not in st.session_state:
     st.session_state.df_coords = pd.DataFrame()
     st.session_state.df_coords_original = pd.DataFrame()
